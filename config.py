f5423d4ec70420528e001cd8240b0b35a5a53862
fizanahmed274-source <fizanahmed274@gmail.com>
2026-08-12 17:26:04 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+CHANNEL_USERNAME = "@Neetmastry"
+GROUP_USERNAME = "@neetmastery0"
+
+CHANNEL_URL = "https://t.me/Neetmastry"
+GROUP_URL = "https://t.me/neetmastery0"
+
+BOT_NAME = "NEET Study Hub 2027"
